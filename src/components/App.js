85dc6a9489f615416308a36f3f8c0c7495b04999
Tyler Cookson <tcookson0805@gmail.com>
2017-07-08 22:23:43 -0700

    added api key

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import API_KEY from '../../config/config.js'
 
 import Header from './header/header.js';
 import Search from './search/search.js';
 import MainVideo from './main_video/main_video.js';
 import VideoList from './video_list/video_list.js';
+
 
 class App extends React.Component {
   render() {
